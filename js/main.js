65b035ad32fbe0754ee12717426af9a1130d9a73
test <135232641+kakashipg@users.noreply.github.com>
2023-10-15 19:49:50 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$(function () {
+
+// slick
+  $('.slick-area').slick({
+    arrows: true,
+    centerMode: true,
+    centerPadding: "10px",
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+
+  $(function () {
+    // セレクタを指定して2つの要素を取得
+    let buyElement = $(".buy-btn");
+    let contactElement = $(".contact-btn");
+  
+    // 両方の要素を初期状態で非表示にする
+    buyElement.hide();
+    contactElement.hide();
+  
+    // ウィンドウのスクロールイベントを監視し、条件を満たすとfadeInさせる
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 150) {
+        buyElement.fadeIn();
+        contactElement.fadeIn();
+      } else {
+        buyElement.fadeOut();
+        contactElement.fadeOut();
+      }
+    });
+  });
+});
+
